a526a7d40c924b57be887a26270be931611b61a0
PhiliLegault <philippe.m.legault96@gmail.com>
2018-12-30 18:33:24 -0500

    fix issue with logs of x,y bearing values

--- Python/fun.sh
+++ Python/fun.sh
@@ -9,6 +9,8 @@ do
     y=`awk '{print $11}' magLine.log`
     x=$( printf "%.0f" $x )
     y=$( printf "%.0f" $y )
+    echo "fun.sh x value $x"
+    echo "fun.sh y value: $y"
     ./tanTest.sh $x $y >> bearing.log
     tail -n 1 bearing.log 
     sleep 3
