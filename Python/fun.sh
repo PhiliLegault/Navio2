f1554a3e45d3d266dd30d4dfb2baaf540e2b526f
PhiliLegault <philippe.m.legault96@gmail.com>
2018-12-30 18:37:27 -0500

    output x.y to standard out

--- Python/fun.sh
+++ Python/fun.sh
@@ -9,10 +9,11 @@ do
     y=`awk '{print $11}' magLine.log`
     x=$( printf "%.0f" $x )
     y=$( printf "%.0f" $y )
-    echo "fun.sh x value $x"
-    echo "fun.sh y value: $y"
-    ./tanTest.sh $x $y >> bearing.log
-    tail -n 1 bearing.log 
+
+    ./tanTest.sh $x $y
+
+    # ./tanTest.sh $x $y >> bearing.log
+    # tail -n 1 bearing.log 
     sleep 3
     
 done
