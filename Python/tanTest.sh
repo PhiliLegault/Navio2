19e7b490a50824d52327019e581db66ab7113bf4
PhiliLegault <philippe.m.legault96@gmail.com>
2018-12-30 01:25:17 -0500

    radian x,y values for tanTest

--- Python/tanTest.sh
+++ Python/tanTest.sh
@@ -6,13 +6,13 @@ y=$2
 
  
 pi=`echo "4*a(1)" | bc -l`
-#radx=`echo "$x*($pi/180)" | bc -l`
-#rady=`echo "$y*($pi/180)" | bc -l`
+radx=`echo "$x*($pi/180)" | bc -l`
+rady=`echo "$y*($pi/180)" | bc -l`
 
 if [[ $y -gt 0 ]]; then 
-    atanRad=`echo "90-(a($x/$y)*(180/$pi))" | bc -l`
+    atanRad=`echo "90-(a($radx/$rady)*(180/$pi))" | bc -l`
 elif [[ $y -lt 0 ]]; then 
-    atanRad=`echo "270-(a($x/$y)*(180/$pi))" | bc -l`
+    atanRad=`echo "270-(a($radx/$rady)*(180/$pi))" | bc -l`
 elif [[ $y -eq 0 ]] && [[ $x -lt 0 ]]; then 
     atanRad="180"
 elif [[ $y -eq 0 ]] && [[ $x -gt 0 ]]; then 
