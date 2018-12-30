c5bcca22b79b9a6313e781dd7f5bb3f96e6f4b4c
PhiliLegault <philippe.m.legault96@gmail.com>
2018-12-30 18:16:24 -0500

    remove rad values from tanTest.sh

--- Python/tanTest.sh
+++ Python/tanTest.sh
@@ -6,24 +6,19 @@ echo "x value: $x"
 echo "y value: $y"
 
 pi=`echo "4*a(1)" | bc -l`
-radx=`echo "$x*($pi/180)" | bc -l`
-rady=`echo "$y*($pi/180)" | bc -l`
-
-echo "radian x value $radx"
-echo "radian y value $rady"
 
 if [[ $y -gt 0 ]]; then 
-    atanRad=`echo "90-(a($radx/$rady)*(180/$pi))" | bc -l`
+    degree=`echo "90-(a($x/$y)*(180/$pi))" | bc -l`
 elif [[ $y -lt 0 ]]; then 
-    atanRad=`echo "270-(a($radx/$rady)*(180/$pi))" | bc -l`
+    degree=`echo "270-(a($x/$y)*(180/$pi))" | bc -l`
 elif [[ $y -eq 0 ]] && [[ $x -lt 0 ]]; then 
-    atanRad="180"
+    degree="180"
 elif [[ $y -eq 0 ]] && [[ $x -gt 0 ]]; then 
-    atanRad="0"
+    degree="0"
 else 
     echo "failed to get degree"
 fi
 
 
-atanRad=$( printf "%.0f" $atanRad )
-echo "00:$atanRad"
+degree=$( printf "%.0f" $degree )
+echo "degree value: 00:$degree"
