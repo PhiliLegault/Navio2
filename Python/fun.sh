42d8156e76af5cdffc938b6a4079fd3f712cc3cc
PhiliLegault <philippe.m.legault96@gmail.com>
2019-01-05 00:10:32 -0500

    test running of mag data in fun script

--- Python/fun.sh
+++ Python/fun.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-python AccelGyroMag.py -i lsm > testData.log &
+python AccelGyroMag.py -i lsm 
+#> testData.log &
 
-while true 
-do  
-    # test values from running original script not represented.
-    # causes issue with chance of double values
-    tail -n 1 testData.log  > magLine.log
-    x=`awk '{print $1}' magLine.log | tr -d '+'`
-    y=`awk '{print $2}' magLine.log`
+# while true 
+# do  
+#     # test values from running original script not represented.
+#     # causes issue with chance of double values
+#     tail -n 1 testData.log  > magLine.log
+#     x=`awk '{print $1}' magLine.log | tr -d '+'`
+#     y=`awk '{print $2}' magLine.log`
     
-    echo $x
-    echo $y
+#     echo $x
+#     echo $y
     
-    #x=$( printf "%.0f" $x )
-    #y=$( printf "%.0f" $y )
+#     #x=$( printf "%.0f" $x )
+#     #y=$( printf "%.0f" $y )
 
-    #./tanTest.sh $x $y
+#     #./tanTest.sh $x $y
 
-    # ./tanTest.sh $x $y >> bearing.log
-    # tail -n 1 bearing.log 
-    sleep 1
+#     # ./tanTest.sh $x $y >> bearing.log
+#     # tail -n 1 bearing.log 
+#     sleep 1
     
-done
+# done
 
