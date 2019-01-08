15a444e0a5f1cbc4303f4fc3963f7273eda86032
PhiliLegault <philippe.m.legault96@gmail.com>
2019-01-08 21:00:42 +0000

    updated on pi

--- Python/fun.sh
+++ Python/fun.sh
@@ -5,50 +5,20 @@ python -u AccelGyroMag.py -i lsm >> testData.txt &
 while true 
 do     
     # get last x and y values 
-    echo "length of file: "
-    echo `wc -l testData.txt`
-    echo " "
-    echo "file contents: "
-    tail -n 1 testData.txt
-    # lenghtOfFile=`wc -l testData.txt | grep -Eo '[0-9]'`
-    # sed "$lenghtOfFile q;d" testData.txt
-    # sed "$(( $lenghtOfFile - 1 )) q;d" testData.txt
-
-
-
-    # test values from running original script not represented.
-    # causes issue with chance of double values
     tail -n 1 testData.txt  > magLine.log
-    y=`awk '{print $1}' magLine.log`
-    x=`awk '{print $2}' magLine.log`
+    x=`awk '{print $1}' magLine.log`
+    y=`awk '{print $2}' magLine.log`
     
     echo "x val $x"
     echo "y val $y"
     
     pi=`echo "4*a(1)" | bc -l`
-    degree=`echo "(a($x/$y)*(180/$pi))" | bc - l`
+    degree=`echo "((a($y/$x) * 180) / $pi)" | bc -l`
 
-
-    # if [[ $(echo "$y > 0" | bc -l) ]]; then 
-    #     degree=`echo "(90-(a($x/$y)*(180/$pi)))" | bc -l`
-    # elif [[ $(echo "$y < 0" | bc -l) ]]; then 
-    #     degree=`echo "(270-(a($x/$y)*(180/$pi)))" | bc -l`
-    # elif [[ $(echo "$y == 0" | bc -l) ]] && [[ $(echo "$x < 0" | bc -l) ]]; then 
-    #     degree="180"
-    # elif [[ $(echo "$y == 0" | bc -l) ]] && [[ $(echo "$x > 0" | bc -l) ]]; then
-    #     degree="0"
-    # else 
-    #     echo "failed to get degree"
-    # fi
 
 
     degree=$( printf "%.0f" $degree )
     echo "degree value: 00:$degree"
-
-    # #./tanTest.sh $x $y
-
-    # # ./tanTest.sh $x $y >> bearing.log
-    # # tail -n 1 bearing.log 
     sleep 1
     
 done
