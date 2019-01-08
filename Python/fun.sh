03d08ccd798a27004ea346a5a21ae416156239dc
elegphi <philippe.legault@ericsson.com>
2019-01-08 10:58:50 -0500

    add degree logic to fun.sh

--- Python/fun.sh
+++ Python/fun.sh
@@ -18,15 +18,30 @@ do
 
     # test values from running original script not represented.
     # causes issue with chance of double values
-    tail -n 1 testData.log  > magLine.log
+    tail -n 1 testData.txt  > magLine.log
     x=`awk '{print $1}' magLine.log | tr -d '+'`
     y=`awk '{print $2}' magLine.log`
     
     echo "x val for tan test: $x"
     echo "y val fro tan test: $y"
     
-    # #x=$( printf "%.0f" $x )
-    # #y=$( printf "%.0f" $y )
+    pi=`echo "4*a(1)" | bc -l`
+
+    if [[ $y -gt 0 ]]; then 
+        degree=`echo "(90-(a($x/$y)*(180/$pi)))*2" | bc -l`
+    elif [[ $y -lt 0 ]]; then 
+        degree=`echo "(270-(a($x/$y)*(180/$pi))*2)" | bc -l`
+    elif [[ $y -eq 0 ]] && [[ $x -lt 0 ]]; then 
+        degree="180"
+    elif [[ $y -eq 0 ]] && [[ $x -gt 0 ]]; then
+        degree="0"
+    else 
+        echo "failed to get degree"
+    fi
+
+
+    degree=$( printf "%.0f" $degree )
+    echo "degree value: 00:$degree"
 
     # #./tanTest.sh $x $y
 
